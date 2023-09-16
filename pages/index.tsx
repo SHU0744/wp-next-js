5583ab56c1c8afb6c8d5608fb6f5e50577bfdb67
SHU0744 <miyavi.senshu@gmail.com>
2023-09-16 13:14:41 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+const Home: NextPage = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>home</div>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount + 1);
+        }}
+      >
+        カウントアップ
+      </button>
+    </>
+  );
+};
+
+export default Home;
