7aa421d0309e9bc6ea733da9510af83c2f4b0ed0
SHU0744 <miyavi.senshu@gmail.com>
2023-09-17 07:48:41 +0900

    ISR×useSWRでリアルタイムデータ取得をする

---
+++ hooks/ser/usePostListSwr.ts
@@ -0,0 +1,15 @@
+import useSWR from "swr";
+import { WpGraphQlPostConst } from "../../constants/WpGraphQlConst";
+import PostType from "../../types/PostType";
+import PostService from "../../services/PostService";
+
+const usePostListSwr = (staticPostList: PostType[]) => {
+  const { data: postList } = useSWR(
+    WpGraphQlPostConst.list,
+    PostService.getList,
+    { fallbackData: staticPostList } //初期値
+  );
+  return postList;
+};
+
+export default usePostListSwr;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import PostService from "../services/PostService";
 import PostType from "../types/PostType";
 
+import usePostListSwr from "../hooks/ser/usePostListSwr";
+
 export async function getStaticProps() {
   const staticPostList = await PostService.getList();
   // console.log("SSGです。");
@@ -9,13 +11,15 @@ export async function getStaticProps() {
     props: {
       staticPostList,
     },
+    revalidate: 10,
   };
 }
 
 const Home: NextPage<{ staticPostList: PostType[] }> = ({ staticPostList }) => {
+  const postList = usePostListSwr(staticPostList);
   return (
     <>
-      {staticPostList.map((post) => {
+      {postList!.map((post) => {
         return <p key={post.id}>{post.title}</p>;
       })}
     </>
