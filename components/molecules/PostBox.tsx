1570bd614555fd1f3742dfb00b424505da8c95f2
SHU0744 <miyavi.senshu@gmail.com>
2023-09-17 20:10:08 +0900

    <Link>を設定

--- components/molecules/PostBox.tsx
+++ components/molecules/PostBox.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PostType from "../../types/PostType";
 import CommonImage from "../atoms/image/CommonImage";
 import CategoryLabel from "../atoms/label/CategoryLabel";
@@ -8,18 +9,30 @@ const PostBox = ({ post }: { post: PostType }) => {
   return (
     <article className="shadow-sm shadow-gray-100">
       <div>
-        <CommonImage
-          src={post.featuredImage.url}
-          alt=""
-          className="w-full h-56"
-        />
+        <Link href={`/post/${post.slug}`}>
+          <a>
+            <CommonImage
+              src={post.featuredImage.url}
+              alt=""
+              className="w-full h-56"
+            />
+          </a>
+        </Link>
       </div>
       <div className="py-4 px-5">
         <div className="flex gap-2 mb-2">
-          <CategoryLabel>{post.category.name}</CategoryLabel>
+          <Link href={`/category/${post.category.slug}`}>
+            <a>
+              <CategoryLabel>{post.category.name}</CategoryLabel>
+            </a>
+          </Link>
           <DateText>{post.date}</DateText>
         </div>
-        <ArticleHeading>{post.title}</ArticleHeading>
+        <Link href={`/post/${post.slug}`}>
+          <a>
+            <ArticleHeading>{post.title}</ArticleHeading>
+          </a>
+        </Link>
         <div
           className="mt-1"
           dangerouslySetInnerHTML={{ __html: post.excerpt }}
